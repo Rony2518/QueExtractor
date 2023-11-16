8654c0e13415e7dc8a68100e6d96662c9dfb04a2
Rony2518 <113433588+Rony2518@users.noreply.github.com>
2023-11-16 17:48:10 +0530

    Add files via upload

--- paperExtractor.py
+++ paperExtractor.py
@@ -21,44 +21,23 @@ def extract_questions_from_pdf(pdf_files):
                 # Use a regular expression to find questions in the text
                 # This assumes that all questions start with "Q." and end with a number
                 # indicating the marks for the question
-                # found_questions = re.findall(
-                #     r"Q\.\d\s\(\w\).*?(?=\nQ\.\d|\nOR|\Z)", text, re.DOTALL
-                # )
-                # Use a regular expression to find the questions.
-                for match in re.finditer(r"Q\.\d\s\(\w\).*?(?=\nQ\.\d|\nOR|\Z)", text, re.DOTALL):
-                    # Add the question to the set.
-                    questions.add(match.group(1))
+                found_questions = re.findall(
+                    r"Q\.\d\s\(\w\).*?(?=\nQ\.\d|\nOR|\Z)", text, re.DOTALL
+                )
 
                 # Add the found questions to the set of questions
                 questions.update(found_questions)
-
     # Write the unique questions to a text file
-    # with open("questionsOfADA.txt", "w", encoding='utf-8') as file:
-    #     for question in questions:
-    #         file.write(question + "\n")
-    # Create a new PDF for the questions.
-    pdf = FPDF()
-
-    # Add a page.
-    pdf.add_page()
-
-    # Set the font.
-    pdf.set_font("Arial", size = 12)
-
-    # Loop through the questions.
-    for question in questions:
-        # Write the question to the PDF.
-        pdf.cell(200, 10, txt = question, ln = True)
-
-    # Save the PDF.
-    pdf.output("questions.pdf")
+    with open("questionsOfADA.txt", "w", encoding='utf-8') as file:
+        for question in questions:
+            file.write(question + "\n")
 
 # List of PDF files to extract questions from
 pdf_files = [
-    "\path\paper1.pdf",
-    "\path\paper2.pdf",
-    "\path\paper3.pdf",
-    "\path\paper4.pdff",
+    "D:\python\paperExtractor\ADA-BE-SUMMER-2022.pdf",
+    "D:\python\paperExtractor\ADA-BE-WINTER-2020.pdf",
+    "D:\python\paperExtractor\ADA-BE-WINTER-2021.pdf",
+    "D:\python\paperExtractor\ADA-BE-WINTER-2022.pdf",
 ]
 
 # Extract questions from the PDF files
